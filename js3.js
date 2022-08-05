d22231e53e137e3cd62d1628c9ed4441fa7dc36f
kimanh78 <kimanhnguyen8532@gmail.com>
2022-08-05 12:08:02 +0700

    nop bai tap lan 2

---
+++ js3.js
@@ -0,0 +1,15 @@
+document.write("Bai 1" + "</br>" + "</br>");
+let i = 10;
+let	f = 20.5;
+let b = true
+let s = 'Ha noi'
+document.write("i ="+ i + "</br>");
+document.write("f ="+ f + "</br>");
+document.write("b ="+ b + "</br>");
+document.write("s ="+ s + "</br>" + "</br>");
+// bai2
+document.write("Bai 2" + "</br>" + "</br>");
+let width =20;
+let height =10;
+let area = width*height;
+document.write("area ="+" "+ area);
